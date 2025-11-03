1925f5b0a011ced2e0ead673da9a798eb0d4daac
franco <mancillafranco71@gmail.com>
2025-11-02 23:08:29 -0300

    test_2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  server: {
+    port: process.env.NODE_ENV === 'production' ? (Number(process.env.PORT) || 80) : 4321,
+    host: true
+  }
 });
