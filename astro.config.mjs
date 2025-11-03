e2db3621f8d2ec28d9d872efce632d32c4e3a6d0
franco <mancillafranco71@gmail.com>
2025-11-02 23:15:17 -0300

    test_3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   output: 'server',
+
   adapter: node({
     mode: 'standalone'
   }),
+
   server: {
     port: process.env.NODE_ENV === 'production' ? (Number(process.env.PORT) || 80) : 4321,
     host: true
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
